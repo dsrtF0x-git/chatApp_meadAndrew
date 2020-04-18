b268a7480dff7981050257cf985de1859f05db8e
dsrtF0x-git <gespenst.serge@yandex.ru>
2020-04-18 04:34:28 -0700

    from windows

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,6 +12,23 @@ const messageTemplate = document.querySelector("#message-template").innerHTML;
 const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
+const autoScroll = () => {
+  const $newMessage = $messages.lastElementChild;
+
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const $newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  const visibleHeight = $messages.offsetHeight;
+  const containerHeight = $messages.scrollHeight;
+
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - $newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+}
+
 socket.on("message", (message) => {
   const html = Mustache.render(messageTemplate, {
     username: message.username,
@@ -19,6 +36,7 @@ socket.on("message", (message) => {
     createdAt: moment(message.createdAt).format("hh:mm:ss a")
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll();
 });
 
 const locationTemplate = document.querySelector("#location-template").innerHTML;
